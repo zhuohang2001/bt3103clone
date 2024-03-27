8000c67d99ba7ed6c5b95778787041a30b407918
chanzhexuan <e0958242@u.nus.edu>
2024-03-27 17:06:41 +0800

    add new listing button and topleftlogo router

--- Wanderer's Marketplace/src/views/Home.vue
+++ Wanderer's Marketplace/src/views/Home.vue
@@ -8,6 +8,7 @@ export default {
   <div id="container">
     <div id="FirstDiv">
       <h1 id="YourPurchases">Your Purchases</h1>
+      <button class="button" id="AddNewListingButton" type="button"><span>Add New Listing</span></button>
     </div>
     
     <div id="SecondDiv" class="scroll">
@@ -18,8 +19,8 @@ export default {
       <h1 id="GoodsYouAreHelpingToBuy">Goods You Are Helping To Buy</h1>
     </div>
 
-    <div id="FourthDiv">
-      <h1>Listings here</h1>
+    <div id="FourthDiv" class="scroll">
+      <h1>Listings hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</h1>
     </div>
   </div>
 </template>
@@ -60,5 +61,43 @@ export default {
     overflow-x: auto;
     overflow-y: hidden;
     white-space: nowrap;
+  }
+
+  #AddNewListingButton {
+    background-color: #fff1e7;
+    font-size: 20px;
+    padding: 4px 20px;
+    border-radius: 12px;
+    border: none;
+    display: inline-block;
+    text-align: center;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin: 5px;
+  }
+
+  .button span {
+    cursor: pointer;
+    display: inline-block;
+    position: relative;
+    transition: 0.5s;
+  }
+
+  .button span:after {
+    content: '\00bb';
+    position: absolute;
+    opacity: 0;
+    top: 0;
+    right: -20px;
+    transition: 0.5s;
+  }
+
+  .button:hover span {
+    padding-right: 25px;
+  }
+
+  .button:hover span:after {
+    opacity: 1;
+    right: 0;
   }
 </style>
