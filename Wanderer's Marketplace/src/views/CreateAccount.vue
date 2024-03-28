f2fc42cd77d660d24cc3172d658ba97fbfa9876d
shawn1802 <e0958597@u.nus.edu>
2024-03-28 17:52:31 +0800

    updated createaccount page
    adjusted the fields for sign up, but alignment not settled

--- Wanderer's Marketplace/src/views/CreateAccount.vue
+++ Wanderer's Marketplace/src/views/CreateAccount.vue
@@ -5,31 +5,61 @@
       </div>
       <div class="signup-form">
         <h2>Create Your Account</h2>
-        <label for="email">Email Address </label><br>
-        <input type="email" id="email" name="email" class = "input-field" v-model="email"><br><br>
-        <label for="password">Password </label><br>
-        <input type="password" id="password" name="password" class = "input-field" v-model="password"><br><br>
-        <input type="checkbox" id="rememberMe" name="rememberMe">
-        <label for="rememberMe"> Remember me</label><br><br>
-        <button class="login-button" @click = "login()">Login</button>
+        <label for="email">Email Address</label><br>
+        <input type="email" id="email" name="email" class="input-field" v-model="email"><br><br>
+        <label for="password">Password</label><br>
+        <input type="password" id="password" name="password" class="input-field" v-model="password"><br><br>
+        <label for="confirmPassword">Confirm Password</label><br>
+        <input type="password" id="confirmPassword" name="confirmPassword" class="input-field" v-model="confirmPassword"><br><br>
+        <label for="country">Country</label><br>
+        <select id="country" name="country" class="input-field" v-model="country">
+          <option value="USA">USA</option>
+          <option value="Canada">Canada</option>
+          <option value="UK">UK</option>
+          <!-- Add more options as needed -->
+        </select><br><br>
+        <label for="cardholderName">Cardholder Name</label><br>
+        <input type="text" id="cardholderName" name="cardholderName" class="input-field" v-model="cardholderName"><br><br>
+        <label for="cardNumber">Card Number</label><br>
+        <input type="text" id="cardNumber" name="cardNumber" class="input-field" v-model="cardNumber"><br><br>
+        <div class="row">
+          <div class="col">
+            <label for="CVV">CVV</label><br>
+            <input type="text" id="CVV" name="CVV" class="input-field" v-model="CVV"><br><br>
+          </div>
+          <div class="col">
+            <label for="expiryDate">Expiry Date</label><br>
+            <input type="text" id="expiryDate" name="expiryDate" class="input-field" v-model="expiryDate"><br><br>
+          </div>
+        </div>
+        <label for="telegramHandle">Telegram Handle</label><br>
+        <input type="text" id="telegramHandle" name="telegramHandle" class="input-field" v-model="telegramHandle"><br><br>
+        <button class="createaccount-button" @click="login()">Create Account</button>
         <div v-if="error" class="error-message">{{ error }}</div>
         <div class="additional-text">
-          <p>Don't have an account? Sign up here</p>
-          <p>Forgot password? Reset here</p>
+          <p>Already have an account?<router-link to="/" class="login-link" >Login here</router-link></p>
         </div>
       </div>
     </div>
   </template>
   
   <script>
-  import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+  import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
   import app from "../firebase.js";
+  
   export default {
     name: 'CreateAccount',
     data() {
       return {
         email: '',
         password: '',
+        confirmPassword: '',
+        country: '',
+        cardholderName: '',
+        cardNumber: '',
+        CVV: '',
+        expiryDate: '',
+        telegramHandle: '',
         error: '',
         user: null,
       };
@@ -49,15 +79,15 @@
     methods: {
       async login() {
         try {
-          // Call the Firebase signInWithEmailAndPassword method to authenticate the user
+          // Call the Firebase createUserWithEmailAndPassword method to create a new user
           const auth = getAuth();
-          await signInWithEmailAndPassword(auth, this.email, this.password)
-          // If authentication is successful, redirect to home
+          await createUserWithEmailAndPassword(auth, this.email, this.password);
+          // If account creation is successful, redirect to home or login page
           this.$router.push('/home');
         } catch (error) {
-          // Handle authentication errors here
+          // Handle account creation errors here
           this.error = error.message;
-          console.error('Error signing in:', error.message);
+          console.error('Error creating account:', error.message);
         }
       }
     }
@@ -97,21 +127,18 @@
     width: 2px; /* Width of the line */
     background-color: #051E55; /* Color of the line */
   }
-  .grey-text{
-    color: #717171;
-  }
-  
-  form{
+  form {
     text-align: center;
     align-items: center;
     margin: auto;
   }
+  
   .input-field {
     width: 300px; /* Adjust the width of the input fields */
     box-sizing: border-box;
   }
   
-  .login-button {
+  .createaccount-button {
     padding: 10px 20px;
     background-color: #051E55;
     color: white;
@@ -125,4 +152,23 @@
   .additional-text {
     text-align: center;
   }
+
+  .login-link {
+    font-size: 15px;
+    color:#051E55;
+  }
+  
+  .row {
+    display: flex;
+  }
+  
+  .col {
+    flex: 1;
+    margin: 0 20px; /* Adjust margin as needed */
+  }
+  #CVV, #expiryDate {
+    width: 100px; /* Calculate width to fit within half of the container width */
+  }
+  
   </style>
+  
